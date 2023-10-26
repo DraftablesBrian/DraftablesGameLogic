a76f835c7edbd5a7b8ce534d7b0aaa3d39943162
0xBrian <124400610+DraftablesBrian@users.noreply.github.com>
2023-10-26 12:30:59 -0400

    Update FieldGoalProbability.cs

--- FieldGoalProbability.cs
+++ FieldGoalProbability.cs
@@ -3,17 +3,31 @@ public class FieldGoalCalculator
     public static int CalculateFieldGoalProbability(int kickPower, int kickAccuracy, int distance)
     {
         // Normalize the stats to be between 0 and 1
-        double normalizedKickPower = kickPower / 100.0;
-        double normalizedKickAccuracy = kickAccuracy / 100.0;
+        double normalizedKickPower = kickPower / 100.0; // CHANGE to class attribute: athlete[i].kickPower
+        double normalizedKickAccuracy = kickAccuracy / 100.0; // Also change
 
         // Calculate the base probability based on the distance
-        double maxFieldGoalDistance = 60.0;  // Example maximum field goal distance
-        double baseProbability = 1 - (distance / maxFieldGoalDistance);
+        double maxFieldGoalDistance = 70.0;  
+        double baseProbability = 1 - (distance / maxFieldGoalDistance); // Not accurate
         
         // Adjust the probability based on the player's kick power and accuracy
-        double adjustedProbability = baseProbability * normalizedKickPower * normalizedKickAccuracy;
+        double adjustedProbability;
+        if (distance <= 30)
+        {
+            adjustedProbability = 1;
+        }
+        else
+        {
+           adjustedProbability = baseProbability * normalizedKickPower * normalizedKickAccuracy;
+        }
         
         // Convert the probability to a percentage and return it
         return (int)(adjustedProbability * 100);
     }
 }
+
+
+/* probability:
+Kick power:
+Kick accuracy:
+Slant: -0.0235x + 1.65
