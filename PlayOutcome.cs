8a6c9135c589fe4ded650b375d0968b91f12f1f1
0xBrian <124400610+DraftablesBrian@users.noreply.github.com>
2023-11-10 14:54:40 -0400

    Update PlayOutcome.cs
    2.5/4 complete

--- PlayOutcome.cs
+++ PlayOutcome.cs
@@ -1,4 +1,4 @@
-
+//Run vs Blitz
 if (offense == OffensivePlay.Run && defense == DefensivePlay.Blitz)
 {
   double avgLinebackerSpeed = AverageStat(linebackers, x => x.Speed);
@@ -19,6 +19,8 @@ if (offense == OffensivePlay.Run && defense == DefensivePlay.Blitz)
   }
 }
 
+//------------------------------------------------------------------------------------
+//Pass vs Nickel
 if (offense == OffensivePlay.Pass && defense == DefensivePlay.Nickel)
 {
   //Speed
@@ -38,6 +40,121 @@ if (offense == OffensivePlay.Pass && defense == DefensivePlay.Nickel)
   double agilityOutcome = avgWideReceiverAgility - avgCornerBackAgility;
   double handEyeOutcome = avgWideReceiverHandEye - avgCornerBackHandEye;
   double gainProbability = (speedOutcome + agilityOutcome + handEyeOutcome) / 10
+  
+    if (gainProbability >= 0.5)
+  {
+    gainProbability = 0.5;
+  }
+    
+}
+
+//------------------------------------------------------------------------------------
+//Option vs. Three-Four -> ADD IN QB HAND EYE?
+if (offense == OffensivePlay.Option && defense == DefensivePlay.ThreeFour)
+{
+  //Speed
+  double avgWideReceiverSpeed = AverageStat(wideReceiver, x => x.Speed);
+  double avgCornerBackSpeed = AverageStat(cornerbacks, x => x.Agility);
+
+  //Agility
+  double avgWideReceiverAgility = AverageStat(wideReceiver, x => x.Agility);
+  double avgCornerBackAgility = AverageStat(cornerbacks, x => x.Agility);
+  
+  //Hand-Eye
+  double avgWideReceiverHandEye = AverageStat(wideReceiver, x => x.HandEye);
+  double avgCornerBackHandEye = AverageStat(cornerbacks, x => x.HandEye);
+
+  //Run
+  //Speed
+  double avgLinebackerSpeed = AverageStat(linebackers, x => x.Speed);
+  double avgRunningBackSpeed = AverageStat(runningBacks, x => x.Speed);
+
+  //agility
+  double avgRunningBackAgility = AverageStat(runningBacks, x => x.Agility);
+  double avgLinebackerAgility = AverageStat(linebackers, x => x.Agility);
+
+  //strength
+  double avgOLinemenStrength = AverageStat(offensiveLinemen, x => x.Strength);
+  double avgDLinemenStrength = AverageStat(defensiveLinemen, x => x.Strength);
+
+  //QB Awareness
+  double avgQbAwareness = AverageStat(quarterback, x => x.Awareness);
+
+  //Outcomes
+  int passSpeedOutcome = avgWideReceiverSpeed - avgCornerBackSpeed;
+  double passAgilityOutcome = avgWideReceiverAgility - avgCornerBackAgility;
+  double passHandEyeOutcome = avgWideReceiverHandEye - avgCornerBackHandEye;
+  double linemenOutcome = avgOLinemenStrength - avgDLinemenStrength;
+  double runSpeedOutcome = avgRunningBackSpeed - avgLinebackerSpeed;
+  double runAgilityOutcome = avgRunningBackAgility - avgLinebackerAgility;
+  double qbAwarenessOutcome = avgQbAwareness;
+
+  //qb awareness should negatively impact outcome if equal to or below 50
+  if (avgQbAwareness <= 50)
+  {
+    qbAwarenessOutcome = qbAwarenessOutcome * -1;
+  }
+
+  //final probability adjustor
+  gainProbability = (passSpeedOutcome + passAgilityOutcome + passHandEyeOutcome + linemenOutcome + runSpeedOutcome + runAgilityOutcome + qbAwarenessOutcome) / 10;
+  
+    if (gainProbability >= 0.5)
+  {
+    gainProbability = 0.5;
+  }
+    
+}
+
+//------------------------------------------------------------------------------------
+//Play action vs Four-Three -> NOT COMPLETE
+if (offense == OffensivePlay.PlayAction && defense == DefensivePlay.FourThree)
+{
+  //Pass
+  //Speed
+  double avgWideReceiverSpeed = AverageStat(wideReceiver, x => x.Speed);
+  double avgCornerBackSpeed = AverageStat(cornerbacks, x => x.Agility);
+
+  //Agility
+  double avgWideReceiverAgility = AverageStat(wideReceiver, x => x.Agility);
+  double avgCornerBackAgility = AverageStat(cornerbacks, x => x.Agility);
+  
+  //Hand-Eye
+  double avgWideReceiverHandEye = AverageStat(wideReceiver, x => x.HandEye);
+  double avgCornerBackHandEye = AverageStat(cornerbacks, x => x.HandEye);
+
+  //Run
+  //Speed
+  double avgLinebackerSpeed = AverageStat(linebackers, x => x.Speed);
+  double avgRunningBackSpeed = AverageStat(runningBacks, x => x.Speed);
+
+  //agility
+  double avgRunningBackAgility = AverageStat(runningBacks, x => x.Agility);
+  double avgLinebackerAgility = AverageStat(linebackers, x => x.Agility);
+
+  //strength
+  double avgOLinemenStrength = AverageStat(offensiveLinemen, x => x.Strength);
+  double avgDLinemenStrength = AverageStat(defensiveLinemen, x => x.Strength);
+
+  //QB Awareness
+  double avgQbAwareness = AverageStat(quarterback, x => x.Awareness);
+
+  //Outcomes
+  int passSpeedOutcome = avgWideReceiverSpeed - avgCornerBackSpeed;
+  double passAgilityOutcome = avgWideReceiverAgility - avgCornerBackAgility;
+  double passHandEyeOutcome = avgWideReceiverHandEye - avgCornerBackHandEye;
+  double linemenOutcome = avgOLinemenStrength - avgDLinemenStrength;
+  double runSpeedOutcome = avgRunningBackSpeed - avgLinebackerSpeed;
+  double runAgilityOutcome = avgRunningBackAgility - avgLinebackerAgility;
+  double qbAwarenessOutcome = avgQbAwareness;
+
+  //qb awareness should negatively impact outcome if equal to or below 50
+  if (avgQbAwareness <= 50)
+  {
+    qbAwarenessOutcome = qbAwarenessOutcome * -1;
+  }
+
+  //final probability adjustor
+  gainProbability = (passSpeedOutcome + passAgilityOutcome + passHandEyeOutcome + linemenOutcome + runSpeedOutcome + runAgilityOutcome + qbAwarenessOutcome) / 10;
   
     if (gainProbability >= 0.5)
   {
